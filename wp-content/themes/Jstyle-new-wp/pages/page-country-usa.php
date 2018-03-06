e5ce088cc73d8297a3435fd63866478e98af90b2
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-03-06 21:59:27 +0900

    Add Page USA

---
+++ wp-content/themes/Jstyle-new-wp/pages/page-country-usa.php
@@ -0,0 +1,59 @@
+<?php
+/*
+template name: country usa
+*/
+__( 'country usa', 'tcd-w' );
+get_header();
+?>
+<main class="l-main">
+    <?php get_template_part( 'template-parts/page-header' ); ?>
+    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
+
+    <div class="purpose-wrapper">
+        <p class="about">
+            ここに、留学の目的前書きが入ります。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストここに、留学の目的前書きが入ります。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストここに、留学の目的前書きが入ります。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストここに、留学の目的前書きが入ります。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストここに、留学の目的前書きが入ります。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </p>
+
+        <div class="images">
+            <ul>
+                <li class="image i-01">
+                    <a href="javascript:void(0);">
+                        <div class="wrap">
+                            <h1 class="title">短期留学</h1>
+                            <span class="text">テキストテキストテキストテキストテキス</span>
+                        </div>
+                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-image.jpg')"></div>
+                    </a>
+                </li>
+                <li class="image i-02">
+                    <a href="javascript:void(0);">
+                        <div class="wrap">
+                            <h1 class="title">短期留学</h1>
+                            <span class="text">テキストテキストテキストテキストテキス</span>
+                        </div>
+                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-image.jpg')"></div>
+                    </a>
+                </li>
+                <li class="image i-03">
+                    <a href="javascript:void(0);">
+                        <div class="wrap">
+                            <h1 class="title">短期留学</h1>
+                            <span class="text">テキストテキストテキストテキストテキス</span>
+                        </div>
+                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-image.jpg')"></div>
+                    </a>
+                </li>
+                <li class="image i-04">
+                    <a href="javascript:void(0);">
+                        <div class="wrap">
+                            <h1 class="title">短期留学</h1>
+                            <span class="text">テキストテキストテキストテキストテキス</span>
+                        </div>
+                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-image.jpg')"></div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
