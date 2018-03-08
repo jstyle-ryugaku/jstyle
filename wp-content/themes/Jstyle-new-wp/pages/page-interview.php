bf2a4b33c32a9b4117540d73fc0366ee153e7c48
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-03-08 20:36:10 +0900

    Update 検索ページ、新しい順、古い順

--- wp-content/themes/Jstyle-new-wp/pages/page-interview.php
+++ wp-content/themes/Jstyle-new-wp/pages/page-interview.php
@@ -3,10 +3,9 @@
 template name: interview
 */
 
-global $custom_search_vars;
-
 get_header();
 $dp_options = get_desing_plus_option();
+$args =  array('order' => 'DESC');
 
 // タグフィルター用ターム配列
 $tags = false;
@@ -22,13 +21,14 @@ if ($custom_search_vars) {
 }
 
 // sort
-if (!empty($_REQUEST['sort']) && in_array($_REQUEST['sort'], array('date_asc', 'date_desc', 'views'))) {
-    $sort = $_REQUEST['sort'];
+if (isset($_GET['sort'])) {
+    $sort = $_GET['sort'];
 } else {
-    $sort = 'date_desc';
+    $sort = 'desc';
 }
-$sort_base_url = remove_query_arg('sort');
-$sort_base_url = preg_replace('#/page/\d+#', '', $sort_base_url);
+
+$args['order'] = $sort;
+
 ?>
 
 <?php get_template_part('template-parts/page-header'); ?>
@@ -47,149 +47,93 @@ $sort_base_url = preg_replace('#/page/\d+#', '', $sort_base_url);
 </div>
 
 <div id="main_col" class="clearfix">
-
-    <div id="left_col" class="custom_search_results">
-
-        <?php
-        if (have_posts() || !empty($_REQUEST['filter_tag'])) {
-            // タグ絞り込み検索表示
-            if ($tags) {
+    <?php
+    if (have_posts() || !empty($_REQUEST['filter_tag'])) {
+        // タグ絞り込み検索表示
+        if ($tags) {
+            ?>
+            <form action="?" method="get" class="archive_filter">
+                <?php
+                foreach (array('post_type', 'page_id', 'cat', 'p', 'sort', 'search_keywords', 'search_keywords_operator', 'search_cat1', 'search_cat2') as $get_key) {
+                    if (!empty($_REQUEST[$get_key])) {
+                        ?>
+                        <input type="hidden" name="<?php echo esc_attr($get_key); ?>"
+                               value="<?php echo esc_attr(stripslashes($_REQUEST[$get_key])); ?>">
+                        <?php
+                    }
+                }
                 ?>
-                <form action="?" method="get" class="archive_filter">
+                <div class="archive_filter_headline rich_font"><?php _e('Refine Search', 'tcd-w'); ?></div>
+                <div class="archive_filter01 archive_filter_tag clearfix">
                     <?php
-                    foreach (array('post_type', 'page_id', 'cat', 'p', 'sort', 'search_keywords', 'search_keywords_operator', 'search_cat1', 'search_cat2') as $get_key) {
-                        if (!empty($_REQUEST[$get_key])) {
-                            ?>
-                            <input type="hidden" name="<?php echo esc_attr($get_key); ?>"
-                                   value="<?php echo esc_attr(stripslashes($_REQUEST[$get_key])); ?>">
-                            <?php
+                    foreach ($tags as $tag) {
+                        $checked = '';
+                        if (!empty($_REQUEST['filter_tag']) && in_array($tag->term_id, $_REQUEST['filter_tag'])) {
+                            $checked = ' checked="checked"';
+                        } else {
+                            $checked = '';
                         }
+                        echo '<label><input type="checkbox" name="filter_tag[]" value="' . esc_attr($tag->term_id) . '"' . $checked . '><span>' . esc_html($tag->name) . '</span></label>' . "\n";
                     }
                     ?>
-                    <div class="archive_filter_headline rich_font"><?php _e('Refine Search', 'tcd-w'); ?></div>
-                    <div class="archive_filter01 archive_filter_tag clearfix">
-                        <?php
-                        foreach ($tags as $tag) {
-                            $checked = '';
-                            if (!empty($_REQUEST['filter_tag']) && in_array($tag->term_id, $_REQUEST['filter_tag'])) {
-                                $checked = ' checked="checked"';
-                            } else {
-                                $checked = '';
-                            }
-                            echo '<label><input type="checkbox" name="filter_tag[]" value="' . esc_attr($tag->term_id) . '"' . $checked . '><span>' . esc_html($tag->name) . '</span></label>' . "\n";
-                        }
-                        ?>
-                    </div>
-                    <div class="button">
-                        <input type="submit" value="<?php _e('Search for', 'tcd-w'); ?>">
-                    </div>
-                </form>
-                <?php
-            }
+                </div>
+                <div class="button">
+                    <input type="submit" value="<?php _e('Search for', 'tcd-w'); ?>">
+                </div>
+            </form>
+            <?php
         }
-        ?>
+    }
+    ?>
 
-        <?php if (have_posts()) : ?>
-            <dl class="archive_sort clearfix">
-                <dt>並べ替え条件</dt>
-                <dd>
-                    <a href="<?php echo esc_attr(add_query_arg('sort', 'date_desc', $sort_base_url)); ?>"<?php if ($sort == 'date_desc') echo ' class="active"'; ?>>新しい順</a>
-                </dd>
-                <dd>
-                    <a href="<?php echo esc_attr(add_query_arg('sort', 'date_asc', $sort_base_url)); ?>"<?php if ($sort == 'date_asc') echo ' class="active"'; ?>>古い順</a>
-                </dd>
-                <dd>
-                    <a href="<?php echo esc_attr(add_query_arg('sort', 'views', $sort_base_url)); ?>"<?php if ($sort == 'views') echo ' class="active"'; ?>>閲覧数順</a>
-                </dd>
-            </dl>
+    <?php if (have_posts()) : ?>
+        <dl class="archive_sort clearfix">
+            <dt>並べ替え条件</dt>
+            <dd>
+                <a href="<?php echo esc_attr(add_query_arg('sort', 'desc', $sort_base_url)); ?>"
+                    <?php if ($sort == 'desc') echo ' class="active"'; ?>>
+                新しい順</a>
+            </dd>
+            <dd>
+                <a href="<?php echo esc_attr(add_query_arg('sort', 'asc', $sort_base_url)); ?>"
+                    <?php if ($sort == 'asc') echo ' class="active"'; ?>>
+                古い順</a>
+            </dd>
+            <dd>
+                <a href="<?php echo esc_attr(add_query_arg('sort', 'views', $sort_base_url)); ?>"
+                    <?php if ($sort == 'views') echo ' class="active"'; ?>>
+                閲覧数順</a>
+            </dd>
+        </dl>
 
-            <?php get_template_part('template-parts/navigation2'); ?>
+        <?php get_template_part('template-parts/navigation2'); ?>
 
-            <ol id="post_list2">
+        <ol id="post_list2">
+            <?php
+            $posts_list = get_posts($args);
+            foreach ( $posts_list as $post ) :
+                setup_postdata( $post );
+            ?>
 
-                <?php while (have_posts()) : the_post(); ?>
-                    <li class="article">
-                        <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" class="clearfix">
-                            <div class="image">
-                                <?php
-                                if (has_post_thumbnail()) {
-                                    if (is_mobile()) {
-                                        the_post_thumbnail('size1');
-                                    } else {
-                                        the_post_thumbnail('size2');
-                                    }
-                                } else {
-                                    if (is_mobile()) {
-                                        echo '<img src="' . get_template_directory_uri() . '/img/common/no_image1.gif" title="" alt="" />';
-                                    } else {
-                                        echo '<img src="' . get_template_directory_uri() . '/img/common/no_image2.gif" title="" alt="" />';
-                                    }
-                                } ?>
-                            </div>
-                            <div class="info">
-                                <?php
-                                $metas = array();
-                                if ($post->post_type == 'post') {
-                                    if ($dp_options['show_categories']) {
-                                        foreach (explode('-', $dp_options['show_categories']) as $cat) {
-                                            if ($cat == 1) {
-                                                $terms = get_the_terms($post->ID, 'category');
-                                                if ($terms && !is_wp_error($terms)) {
-                                                    foreach ($terms as $term) {
-                                                        $metas['category'][] = '<span class="cat-category" data-href="' . get_term_link($term) . '" title="' . esc_attr($term->name) . '">' . esc_html($term->name) . '</span>';
-                                                    }
-                                                    $metas['category'] = '<li class="cat">' . implode('', $metas['category']) . '</li>';
-                                                }
-                                            } elseif (!empty($dp_options['use_category' . $cat])) {
-                                                $terms = get_the_terms($post->ID, $dp_options['category' . $cat . '_slug']);
-                                                if ($terms && !is_wp_error($terms)) {
-                                                    foreach ($terms as $term) {
-                                                        $metas['category' . $cat][] = '<span class="cat-' . esc_attr($dp_options['category' . $cat . '_slug']) . '" data-href="' . get_term_link($term) . '" title="' . esc_attr($term->name) . '">' . esc_html($term->name) . '</span>';
-                                                    }
-                                                    $metas['category' . $cat] = '<li class="cat">' . implode('', $metas['category' . $cat]) . '</li>';
-                                                }
-                                            }
-                                        }
-                                    }
+                <div>
+                    <?php the_date(); ?>
+                    <br />
+                    <?php the_title(); ?>
+                    <?php the_excerpt(); ?>
+                </div>
+            <br>
 
-                                } elseif ($post->post_type == $dp_options['introduce_slug']) {
-                                    if ($dp_options['show_introduce_categories']) {
-                                        foreach (explode('-', $dp_options['show_introduce_categories']) as $cat) {
-                                            if (!empty($dp_options['use_introduce_category' . $cat])) {
-                                                $terms = get_the_terms($post->ID, $dp_options['introduce_category' . $cat . '_slug']);
-                                                if ($terms && !is_wp_error($terms)) {
-                                                    $term = array_shift($terms);
-                                                    $metas[] = '<li class="cat"><span class="cat-' . esc_attr($dp_options['introduce_category' . $cat . '_slug']) . '" data-href="' . get_term_link($term) . '" title="' . esc_attr($term->name) . '">' . esc_html($term->name) . '</span></li>';
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+            <?php
+            endforeach;
+            wp_reset_postdata();
+            ?>
+        </ol>
 
-                                if ($metas) {
-                                    echo '    <ul class="meta clearfix">' . implode('', $metas) . '</ul>' . "\n";
-                                }
-                                ?>
-                                <h3 class="title"><?php trim_title(38); ?></h3>
-                                <p class="excerpt"><?php new_excerpt(118); ?></p>
-                            </div>
-                        </a>
-                    </li>
-                <?php endwhile; ?>
-
-            </ol><!-- END #post_list2 -->
-
-            <?php get_template_part('navigation2'); ?>
-
-        <?php else: ?>
-            <p class="no_post"><?php _e('There is no registered post.', 'tcd-w'); ?></p>
-        <?php endif; ?>
-
-    </div><!-- END #left_col -->
-
-    <?php get_sidebar(); ?>
-
-</div><!-- END #main_col -->
+        <?php get_template_part('navigation2'); ?>
+    <?php else: ?>
+        <p class="no_post"><?php _e('There is no registered post.', 'tcd-w'); ?></p>
+    <?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
 
