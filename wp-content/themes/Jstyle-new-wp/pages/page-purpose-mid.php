8460aa1091afe09fc1d08758812e6be13e1cd0a9
hori-design <horimasa09.y10.n01+github@gmail.comm>
2018-03-10 14:32:18 +0900

    Add purpose-mid page

---
+++ wp-content/themes/Jstyle-new-wp/pages/page-purpose-mid.php
@@ -0,0 +1,135 @@
+<?php
+/*
+template name: purpose-mid
+*/
+__( 'purpose-mid', 'tcd-w' );
+get_header();
+?>
+<main class="l-main">
+    <?php get_template_part( 'template-parts/page-header' ); ?>
+    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
+
+    <div class="purpose-detail-wrapper">
+
+        <!-- 概要 -->
+        <section class="pd-about">
+            <h1 class="title">中期留学</h1>
+            <p class="text">
+                中期留学とは、3か月から９か月までの期間で行う留学です。語学力を向上させたり、外国で何かを学ぶには十分な期間です。「本気で話せるようになりたい！」「大学でこれが学びたい！」という強い意志がある方は、ぜひ中期で留学へ行きましょう。
+            </p>
+        </section>
+
+        <!-- 中期留学は学べることがたくさん！ -->
+        <section class="pd-point">
+            <h2 class="title">中期留学は学べることがたくさん！</h2>
+            <ul class="point-wrapper">
+                <li class="p-01">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-01.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">語学力の向上！</h3>
+                        <p class="text">
+                            中期間であれば、努力次第でいくらでも語学力を向上させることができます。本気で勉強をすれば、日常では困らない程度の会話をできるようになるでしょう。
+                        </p>
+                    </div>
+                </li>
+                <li class="p-02">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-02.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">他国に友達ができる！</h3>
+                        <p class="text">
+                            クラスメイトやホストファミリーと打ち解けていくための時間的余裕があります。長い間一緒に過ごした人と帰国後も良い関係が続くケースは少なくありません。
+                        </p>
+                    </div>
+                </li>
+                <li class="p-03">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-03.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">憧れの海外生活を体験！</h3>
+                        <p class="text">
+                            映画に出てくるような都市で外国人と暮らせば、旅行では味わえない体験ができます。新しくてワクワクする日々が待っているでしょう。
+                        </p>
+                    </div>
+                </li>
+                <li class="p-04">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-04.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">留学先の観光地を楽しむ！</h3>
+                        <p class="text">
+                            留学は時間がたくさんあります。空いている時間で憧れの有名スポットへ気軽に行くことができます。
+                        </p>
+                    </div>
+                </li>
+                <li class="p-05">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-05.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">ボランティ・インターンシップに挑戦！</h3>
+                        <p class="text">
+                            世界には、日本とは違った価値観や文化、コミュニケーションがあります。ボランティア・インターンシップを通して「世界の中での自分」を感じることが出来ます。ぜひ、挑戦してみましょう！
+                        </p>
+                    </div>
+                </li>
+                <li class="p-06">
+                    <img class="p-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/purpose-mid/point-06.jpg">
+                    <div class="p-wrapper">
+                        <h3 class="title">本場で趣味を楽しむ！</h3>
+                        <p class="text">
+                            語学の他にも、ダイビングやラグビー、ダンスやミュージカルなど様々なものを体験できます。自分の趣味を本場で試してみましょう。
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+        <!-- 中期留学Q&A -->
+        <section class="pd-qa">
+            <h2 class="title">中期留学Q&A</h2>
+            <ul class="pa-wrapper">
+                <li class="qa-01">
+                    <div class="inner">
+                        <span class="question">
+                            <p class="text">語学力はどれくらい必要？</p>
+                        </span>
+                            <span class="answer">
+                            <p class="text">語学力があるにこしたことはないですが、なくても大丈夫です。積極的に会話をし、短期間でできる最大限の努力をしましょう。語学力があるにこしたことはないですが、なくても大丈夫です。積極的に会話をし、短期間でできる最大限の努力をしましょう。
+                        </span>
+                    </div>
+                </li>
+                <li class="qa-02">
+                    <div class="inner">
+                        <span class="question">
+                            <p class="text">語学力はどれくらい必要？</p>
+                        </span>
+                        <span class="answer">
+                            <p class="text">語学力があるにこしたことはないですが、なくても大丈夫です。積極的に会話をし、短期間でできる最大限の努力をしましょう。</p>
+                        </span>
+                </li>
+                <li class="qa-03">
+                    <div class="inner">
+                        <span class="question">
+                            <p class="text">語学力はどれくらい必要？</p>
+                        </span>
+                        <span class="answer">
+                            <p class="text">語学力があるにこしたことはないですが、なくても大丈夫です。積極的に会話をし、短期間でできる最大限の努力をしましょう。</p>
+                        </span>
+                </li>
+                <li class="qa-04">
+                    <div class="inner">
+                        <span class="question">
+                            <p class="text">語学力はどれくらい必要？</p>
+                        </span>
+                        <span class="answer">
+                            <p class="text">語学力があるにこしたことはないですが、なくても大丈夫です。積極的に会話をし、短期間でできる最大限の努力をしましょう。</p>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+        <!-- Jstyleで中期留学しました！ -->
+        <section class="pd-experience">
+            <h2 class="title">Jstyleで中期留学しました！</h2>
+            あああああ
+        </section>
+    </div>
+</main>
+<?php get_footer(); ?>
