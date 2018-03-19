6a1ee97afbf57b6eed9d4ce04e6b38b2dd0c047a
hori-design <horimasa09.y10.n01+github@gmail.comm>
2018-03-19 15:29:19 +0900

    TOPページの、スライド・LINEで相談ボタンの修正

--- wp-content/themes/Jstyle-new-wp/template-parts/hero-header.php
+++ wp-content/themes/Jstyle-new-wp/template-parts/hero-header.php
@@ -13,8 +13,10 @@
             <!--
 
             注　意
-            ここに挿入する画像の縦横比(サイズ)は、PC用、スマホ・タブレット用それぞれで統一してください
-            スライドは何枚でも増やせます
+            ・ここに挿入する画像の縦横比(サイズ)は、PC用、スマホ・タブレット用それぞれで統一してください
+            ・スライドは何枚でも増やせます
+            ・スマホ,タブレット対応する場合は、この要素内の .item クラスが付いたa要素のstyle属性値のコメントアウトを外してください
+            　CSSファイルの該当箇所のコメントアウトも外してください
 
             記入例
             <a class="item" href="javascript:void(0);" style="background-image: url('スマホ, タブレット用画像のパス')">
@@ -23,19 +25,19 @@
 
             -->
             <!-- 1枚目 -->
-            <a class="item" href="javascript:void(0);" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg')">
+            <a class="item" href="javascript:void(0);" style="/* background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg') */">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/header.jpg">
             </a>
             <!-- 2枚目 -->
-            <a class="item" href="javascript:void(0);" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg')">
+            <a class="item" href="javascript:void(0);" style="/* background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg') */">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/header.jpg">
             </a>
             <!-- 3枚目 -->
-            <a class="item" href="javascript:void(0);" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg')">
+            <a class="item" href="javascript:void(0);" style="/* background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg') */">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/header.jpg">
             </a>
             <!-- 4枚目 -->
-            <a class="item" href="javascript:void(0);" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg')">
+            <a class="item" href="javascript:void(0);" style="/* background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/top/header_middle.jpg') */">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/header.jpg">
             </a>
         </div>
