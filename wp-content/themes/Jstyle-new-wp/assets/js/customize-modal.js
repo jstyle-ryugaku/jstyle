5dc9bd7f6a914da25d2ff6054c9dde49910d07ef
su1fur <ihukcl.9748@gmail.com>
2018-03-24 23:00:55 +0900

    Add forework

--- wp-content/themes/Jstyle-new-wp/assets/js/customize-modal.js
+++ wp-content/themes/Jstyle-new-wp/assets/js/customize-modal.js
@@ -1,23 +1,125 @@
 $ = jQuery;
-$(function () {
-    $('.js-show-modal').on('click', showModal);
-    $('.js-close-modal').on('click', hideModal);
 
-    function showModal(event) {
-        event.preventDefault();
+$('.js-show-modal').on('click', showModal);
+$('.js-close-modal').on('click', hideModal);
+$('.js-on-select-period').on('click', onSelectPeriod);
+$('.js-on-select-country').on('click', onSelectCountry);
+$('.js-on-prev').on('click', toPrevPage);
+$('.js-on-next').on('click', toNextPage);
+$('.js-on-to-top').on('click', toTopPage);
 
-        var $modalWin = $('.customize-modal');
 
-        $modalWin
-            .removeClass('hide')
-            .addClass('show');
+var stepNumber = 1;
+var currentNumber = 0;
+var totalCost = 0;
+var customizeType = null;
+var isCountryPage = false;
+var isTypePage = false;
+var isPeriodPage = false;
+const $topPage = $('.top-page');
+var $currentPage = $topPage;
+var $nextPage;
+var $prevPage;
+
+function showModal(event) {
+    event.preventDefault();
+    var $modalWin = $('.customize-modal');
+    $modalWin
+        .removeClass('hide')
+        .addClass('show');
+}
+
+function hideModal() {
+    $('#modal-window')
+        .removeClass('show')
+        .addClass('hide');
+}
+
+function showPage(pageClass) {
+    pageClass
+        .removeClass('hide')
+        .addClass('show');
+}
+
+function hidePage(pageClass) {
+    pageClass
+        .removeClass('show')
+        .addClass('hide');
+}
+
+function onSelectPeriod() {
+    $nextPage = $('.select-period');
+    customizeType = 'period';
+    toNextPage();
+}
+
+function onSelectCountry() {
+    $nextPage = $('.select-country');
+    customizeType = 'country';
+    toNextPage();
+}
+
+function toNextPage() {
+    hidePage(stepNumberToPageClass(stepNumber));
+    showPage(stepNumberToPageClass(stepNumber + 1));
+    stepNumber++;
+    $(stepNumberToPageClass(stepNumber)).find('.step__number').text('0' + stepNumber);
+    $(stepNumberToPageClass(stepNumber)).find('.foreword').text(stepNumberToForeword(stepNumber));
+}
+
+function toPrevPage() {
+    hidePage(stepNumberToPageClass(stepNumber));
+    showPage(stepNumberToPageClass(stepNumber - 1));
+    stepNumber--;
+    $(stepNumberToPageClass(stepNumber)).find('.step__number').text('0' + stepNumber);
+    $(stepNumberToPageClass(stepNumber)).find('.foreword').text(stepNumberToForeword(stepNumber));
+}
+
+function toTopPage() {
+    hidePage(stepNumberToPageClass(stepNumber));
+    showPage($topPage);
+    stepNumber = 1;
+    $(stepNumberToPageClass(stepNumber)).find('.step__number').text('0' + stepNumber);
+}
+
+function stepNumberToPageClass(stepNumber) {
+    if (stepNumber === 1) {
+        return $('.top-page');
+    } else if(stepNumber === 5) {
+        return $('.result');
     }
-
-    function hideModal() {
-
-        $('#modal-window')
-            .removeClass('show')
-            .addClass('hide');
-
+    if (customizeType === 'period') {
+        switch (stepNumber) {
+            case 2 :
+                return $('.select-period');
+            case 3 :
+                return $('.select-type');
+            case 4 :
+                return $('.select-country');
+        }
+    } else if (customizeType === 'country') {
+        switch (stepNumber) {
+            case 2 :
+                return $('.select-country');
+            case 3 :
+                return $('.select-period');
+            case 4 :
+                return $('.select-type');
+        }
     }
-});
+}
+
+function stepNumberToForeword(stepNumber) {
+    switch (stepNumber) {
+        case 1 :
+            return '';
+        case 2 :
+            return '最初に';
+        case 3 :
+            return '次に';
+        case 4 :
+            return '最後に';
+        case 5 :
+            return '';
+    }
+}
--- wp-content/themes/Jstyle-new-wp/components/customize-modal.php
+++ wp-content/themes/Jstyle-new-wp/components/customize-modal.php
@@ -34,16 +34,97 @@
             <p class="step__number">01</p>
         </div>
         <div class="select-and-next-container">
-            <div class="select-and-next">留学期間で選ぶ</div>
-            <div class="select-and-next">留学先で選ぶ</div>
+            <div class="select-and-next js-on-select-period">留学期間で選ぶ</div>
+            <div class="select-and-next js-on-select-country">留学先で選ぶ</div>
         </div>
     </div>
-    <div class="flex-item select-period">
+    <div class="flex-item select-period hide">
+        <div class="step">
+            <p class="step__number">02</p>
+        </div>
+        <p class="customize-modal__title">
+            <i class="far fa-check-square"></i>
+            <span class="foreword"></span>ご希望の留学期間を入力してください
+        </p>
+        <div class="select-date">
+            <label></label>
+            <input class="input-date" type="date" name="date">
+            <div class="">から<input class="input-period" type="number">ヶ月間</div>
+        </div>
+        <div class="prev-and-next-container">
+            <div class="btn prev js-on-prev">戻る</div>
+            <div class="btn next js-on-next">次へ</div>
+        </div>
     </div>
-    <div class="flex-item select-country">
+    <div class="flex-item select-country hide">
+        <div class="step">
+            <p class="step__number">02</p>
+        </div>
+        <p class="customize-modal__title">
+            <i class="far fa-check-square"></i>
+            <span class="foreword"></span>国を選んでください
+        </p>
+        <div class="select-country">
+            <input type="radio" name="country" id="america"><label for="america">アメリカ</label>
+            <input type="radio" name="country" id="canada"><label for="canada">カナダ</label>
+            <input type="radio" name="country" id="australia"><label for="australia">オーストラリア</label>
+            <input type="radio" name="country" id="UK"><label for="UK">イギリス</label>
+            <input type="text" name="country" id="other" placeholder="その他">
+        </div>
+        <div class="prev-and-next-container">
+            <div class="btn prev js-on-prev">戻る</div>
+            <div class="btn next js-on-next">次へ</div>
+        </div>
     </div>
-    <div class="flex-item select-type">
+    <div class="flex-item select-type hide">
+        <div class="step">
+            <p class="step__number">02</p>
+        </div>
+        <p class="customize-modal__title">
+            <i class="far fa-check-square"></i>
+            <span class="foreword"></span>留学の種類を選んでください
+        </p>
+        <div class="select-type">
+            <input type="radio" name="type" id="working-holiday"><label for="working-holiday">ワーキングホリデー</label>
+            <input type="radio" name="type" id="short"><label for="short">短期留学</label>
+            <input type="radio" name="type" id="university"><label for="university">大学</label>
+            <input type="radio" name="type" id="home-stay"><label for="home-stay">ホームステイ</label>
+            <input type="text" name="type" id="other" placeholder="その他">
+        </div>
+        <div class="prev-and-next-container">
+            <div class="btn prev js-on-prev">戻る</div>
+            <div class="btn next js-on-next">次へ</div>
+        </div>
     </div>
-    <div class="flex-item result">
+    <div class="flex-item result hide">
+        <div>
+            <p class="customize-modal__title">
+                <i class="far fa-check-square"></i>
+                あなたの留学費用は・・・
+            </p>
+            <div>
+                <p><span>約</span>100万円~200万円<span>です</span></span></p>
+            </div>
+            <p>※値段は、ヒアリング結果によって、大きく上下する場合がございます。あらかじめご了承ください。</p>
+        </div>
+        <div>
+            <p class="customize-modal__title">
+                <i class="far fa-check-square"></i>
+                もっと詳しい留学費用を知りたい方は
+            </p>
+            <div class="o-01 o-line">
+                <a href="javascript:void(0);">
+                    <i class="fab fa-line"></i>
+                    <p class="text bold">LINEで相談する</p>
+                </a>
+            </div>
+        </div>
+        <div>
+            <p class="customize-modal__title">
+                <i class="far fa-check-square"></i>
+                他の留学プランを知りたい方は、
+            </p>
+            <div class="js-on-to-top">初めからやりなおす</div>
+        </div>
     </div>
 </div>
