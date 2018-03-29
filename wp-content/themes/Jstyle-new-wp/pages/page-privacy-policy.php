0930555f241e6e4ac8c6bf82c80b317f6567eb2c
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-03-29 15:10:10 +0900

    Add Page-Privacy-Policy

---
+++ wp-content/themes/Jstyle-new-wp/pages/page-privacy-policy.php
@@ -0,0 +1,12 @@
+<?php
+/*
+template name: privacy-policy
+*/
+__( 'purpose', 'tcd-w' );
+get_header();
+?>
+<main class="l-main">
+    <?php get_template_part( 'template-parts/page-header' ); ?>
+    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
+</main>
+
