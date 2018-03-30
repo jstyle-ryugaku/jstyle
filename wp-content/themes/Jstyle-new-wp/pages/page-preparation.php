0efbc828b93a9313807444beccd0d2708fd3d04e
su1fur <ihukcl.9748@gmail.com>
2018-03-31 01:02:42 +0900

    Add age page

--- wp-content/themes/Jstyle-new-wp/pages/page-preparation.php
+++ wp-content/themes/Jstyle-new-wp/pages/page-preparation.php
@@ -2,12 +2,22 @@
 /*
 template name: preparation
 */
-__( 'preparation', 'tcd-w' );
+__('preparation', 'tcd-w');
 get_header();
+
+$life_info_args = array(
+    'post_status' => 'publish',
+    'post_type' => 'life-info',
+    'orderby' => 'post_date',
+    'order' => 'DESC'
+);
+
+$life_info_query = new WP_Query($life_info_args);
+
 ?>
 <main class="l-main">
-    <?php get_template_part( 'template-parts/page-header' ); ?>
-    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
+    <?php get_template_part('template-parts/page-header'); ?>
+    <?php get_template_part('template-parts/breadcrumb'); ?>
 
     <div class="preparation-page-wrapper">
         <h2 class="pp-title">
@@ -20,16 +30,15 @@ get_header();
             <h3 class="title">はじめに</h3>
             <div class="text-box">
                 <p class="text">
-                    こんにちは。留学エージェント Jstyle です。留学を決意したあなた、留学を考えているあなた、留学に行く前に知ってほしいことをお伝えするために、このページをまとめました。知っていること、始めて知ること、いろいろあると思いますが、留学する人すべてに知ってほしい知識や心構えを解説しています。適切な知識を身につければ、あなたの留学の成功や充実度がぐっと高まることは間違いありません。まっさらな気持ちですべての記事を読んでいただければと思います。
+                    こんにちは。留学エージェント Jstyle
+                    です。留学を決意したあなた、留学を考えているあなた、留学に行く前に知ってほしいことをお伝えするために、このページをまとめました。知っていること、始めて知ること、いろいろあると思いますが、留学する人すべてに知ってほしい知識や心構えを解説しています。適切な知識を身につければ、あなたの留学の成功や充実度がぐっと高まることは間違いありません。まっさらな気持ちですべての記事を読んでいただければと思います。
                 </p>
                 <p class="text">
                     まずは、実際に留学に行き、その経験を活かしている方の体験談を読んでみてください。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">留学体験記</a>
-                <a href="javascript:void(0);">留学体験記</a>
-                <a href="javascript:void(0);">留学体験記</a>
+                <?php echo get_interview_tags(); ?>
             </div>
         </section>
 
@@ -41,7 +50,7 @@ get_header();
                     そもそも、留学をするとどんないいことがあるのでしょうか。語学習得、キャリアアップ、学生時代の貴重な想いで作り・・・留学の目的は人によって、違うとは思いますが、実際に留学にいった人の声を聞きながら留学をするとどんないいことがあるか、再確認してみましょう。</p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">留学生が振り返る、"留学にいって良かったこと"</a>
+                <?php echo get_preparation_tags(1); ?>
             </div>
         </section>
 
@@ -53,13 +62,12 @@ get_header();
                     留学の成功は、留学前の語学学習で決まる。
                 </p>
                 <p class="text">
-                    いよいよ留学への決意を固めたあなた。まだ見ぬ世界に一歩踏み出すことに、ワクワクも不安も入り混じった感覚ではないでしょうか。多くの方が誤解していることがあります。それは、”海外で生活すれば、英語はしゃべれるようになる”ということ。たしかに、長期間海外に滞在すれば、少しずつ環境に適応し、語学力は向上します。しかし、まったく英語が喋れない状態でいきなりそのような環境に身をおいても、もちろん最初はうまくコミュニケーションはできません。 現地の会話スピードへの慣れ、必要な語彙力/文法力の習得には一定の期間が必要であるためです。準備不足で、語学力も不十分な状態で、ただ漫然と海外にいっても成長速度は遅いのです。逆にしっかりとした英語の土台を作っておけば、たとえ期間の短い短期留学であっても、留学期間をしっかりと”実践のコミュニケーション”にあてることができ、しっかりとした語学力が身につきます。
+                    いよいよ留学への決意を固めたあなた。まだ見ぬ世界に一歩踏み出すことに、ワクワクも不安も入り混じった感覚ではないでしょうか。多くの方が誤解していることがあります。それは、”海外で生活すれば、英語はしゃべれるようになる”ということ。たしかに、長期間海外に滞在すれば、少しずつ環境に適応し、語学力は向上します。しかし、まったく英語が喋れない状態でいきなりそのような環境に身をおいても、もちろん最初はうまくコミュニケーションはできません。
+                    現地の会話スピードへの慣れ、必要な語彙力/文法力の習得には一定の期間が必要であるためです。準備不足で、語学力も不十分な状態で、ただ漫然と海外にいっても成長速度は遅いのです。逆にしっかりとした英語の土台を作っておけば、たとえ期間の短い短期留学であっても、留学期間をしっかりと”実践のコミュニケーション”にあてることができ、しっかりとした語学力が身につきます。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">留学前の英語勉強法</a>
-                <a href="javascript:void(0);">自分の考えをしっかり伝える大切さ</a>
-                <a href="javascript:void(0);">体験談：留学を成功させた留学生が日本にいるときにどれくらい努力をしているか</a>
+                <?php echo get_preparation_tags(2); ?>
             </div>
         </section>
 
@@ -82,13 +90,7 @@ get_header();
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">留学タイプ1. 語学留学の基礎知識</a>
-                <a href="javascript:void(0);">留学タイプ2. 大学進学・編入の基礎知識</a>
-                <a href="javascript:void(0);">留学タイプ3. ワーキングホリデーの基礎知識</a>
-                <a href="javascript:void(0);">留学の3種類の滞在先</a>
-                <a href="javascript:void(0);">語学学校の評価体系</a>
-                <a href="javascript:void(0);">認定校留学に関する注意点</a>
-                <a href="javascript:void(0);">大学留学生、必見！コミュニティカレッジを活用せよ！</a>
+                <?php echo get_preparation_tags(3); ?>
             </div>
         </section>
 
@@ -97,15 +99,11 @@ get_header();
             <h3 class="title">留学の持ち物　基礎知識</h3>
             <div class="text-box">
                 <!-- 下記文章は、開発者が追加 -->
-                <p class="text">
-                    留学の際の持ち物についてです。
-                </p>
+                <p class="text">いざ、留学しようと決意した際に、最初に悩むのは留学の持ち物。何を日本から持っていくべきで、何を持っていなかなくていいのか？</p>
+                <p class="text">大切な持参品についての基礎知識をまとめました。</p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">持ち物チェックリスト</a>
-                <a href="javascript:void(0);">留学に必要なクレジットカードの基礎知識</a>
-                <a href="javascript:void(0);">留学に必要な携帯・スマホの知識</a>
-                <a href="javascript:void(0);">これだけは知っておいて！日本で準備して持っていくべきもの、持っていかなくてもなんとかなるもの</a>
+                <?php echo get_preparation_tags(4); ?>
             </div>
         </section>
 
@@ -115,17 +113,11 @@ get_header();
             <div class="text-box">
                 <!-- 下記文章は、開発者が追加 -->
                 <p class="text">
-                    留学の際のお金についてです。
+                    やっぱり一番気になるのが、留学にかかるお金。留学にはいきたいけど、なるべく費用は抑えたい・・・そんな方のための、賢いお金の知識をまとめました。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">奨学金を活用しよう</a>
-                <a href="javascript:void(0);">やっぱり気になる、留学滞在中のお金の知識</a>
-                <a href="javascript:void(0);">アメリカ留学中の節約術</a>
-                <a href="javascript:void(0);">オーストラリア留学中の節約術</a>
-                <a href="javascript:void(0);">カナダ留学中の節約術</a>
-                <a href="javascript:void(0);">イギリス留学中の節約術</a>
-                <a href="javascript:void(0);">少しでも費用を抑えたいなら、ビザ申請を自分でチャレンジしてみよう</a>
+                <?php echo get_preparation_tags(5); ?>
             </div>
         </section>
 
@@ -135,13 +127,11 @@ get_header();
             <div class="text-box">
                 <!-- 下記文章は、開発者が追加 -->
                 <p class="text">
-                    留学中のいざというときの知恵です。
+                    どれだけ気を付けていても、留学にはトラブルはつきもの。万が一の病気やケガが、海外滞在中に起こってしまったらどうすればいいか。不安になることもあると思います。そのために事前に知っておいてほしいことをまとめました。。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">留学に必要な保険の知識</a>
-                <a href="javascript:void(0);">留学に必要な予防接種の知識</a>
-                <a href="javascript:void(0);">アメリカで万が一、病気やケガをしてしまったときは？</a>
+                <?php echo get_preparation_tags(6); ?>
             </div>
         </section>
 
@@ -151,13 +141,11 @@ get_header();
             <div class="text-box">
                 <!-- 下記文章は、開発者が追加 -->
                 <p class="text">
-                    留学から帰ってきた後のキャリアについてです。
+                    留学をしたのちに、どのようなキャリアパスを歩むのか？就活や転職など、留学体験がキャリア形成にどのような影響を及ぼし、どのようなメリットがあるのか、まとめました。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">日本での留学キャリアの活かし方</a>
-                <a href="javascript:void(0);">留学キャリア形成の体験談</a>
-                <a href="javascript:void(0);">ボストンキャリアフォーラムを活用して、仕事をゲット！</a>
+                <?php echo get_preparation_tags(7); ?>
             </div>
         </section>
 
@@ -167,12 +155,11 @@ get_header();
             <div class="text-box">
                 <!-- 下記文章は、開発者が追加 -->
                 <p class="text">
-                    実際に留学をした人の体験談です。
+                    実際に留学に行き、留学体験をもとに成功をおさめている先輩たちの体験談をまとめました。
                 </p>
             </div>
             <div class="link">
-                <a href="javascript:void(0);">かずさん 体験談</a>
-                <a href="javascript:void(0);">Gyunさん　体験談</a>
+                <?php echo get_interview_tags(); ?>
             </div>
         </section>
     </div>
