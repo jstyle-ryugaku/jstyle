f929c162f9b2a05a3f9011e7b9ea139655b8fa02
Tatsuya Ichikawa <oral.dr31@gmail.com>
2018-04-26 20:39:01 +0900

    Js yet

---
+++ wp-content/themes/Jstyle-new-wp/assets/js/age.js
@@ -0,0 +1,50 @@
+(function () {
+    "use strict";
+
+    var $ = jQuery,
+        // event_name = (window.ontouchstart === null) ? 'touchstart' : 'click',
+        event_name = 'click',
+        flag_up = false,
+        flag_open = false,
+        timer = false,
+
+        add_sele = '.age-fixed-footer', // クラス付与先
+        button_sele = '.age-fixed-footer .age-button',    // ボタンのセレクタ
+        cover_sele = '.age-fixed-footer .age-body-cover', // bodyを覆うカバーのセレクタ
+        open_class = 'ff-open', // 選択画面を開く場合に付与するクラス名
+        up_class = 'ff-up',     // 問い合わせボタンを表示する場合に付与するクラス名
+
+        interval = 500;
+
+    /* 詳細表示・非表示 */
+    $(button_sele + ', ' + cover_sele).on(event_name, function (e) {
+        e.preventDefault();
+        flag_open = !flag_open;
+        $('.l-header').css('z-index', flag_open ? 499 : 1000);
+        $(add_sele).toggleClass(open_class);
+        return false;
+    });
+
+    /* ボタンの表示・非表示 */
+    $(window).on({
+        'load': function () {
+            if (!flag_up) {
+                $(add_sele).addClass(up_class);
+                flag_up = true;
+            }
+        },
+        'scroll': function () {
+            if (flag_open) return false;
+            if (flag_up) {
+                $(add_sele).removeClass(up_class);
+                flag_up = false;
+            } else {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function () {
+                $(add_sele).addClass(up_class);
+                flag_up = true;
+            }, interval);
+        }
+    });
+}());
--- wp-content/themes/Jstyle-new-wp/pages/page-age.php
+++ wp-content/themes/Jstyle-new-wp/pages/page-age.php
@@ -2,141 +2,135 @@
 /*
 template name: age
 */
-<<<<<<< HEAD
+
 __('age', 'tcd-w');
 get_header();
 ?>
-<main class="l-main">
-    <?php get_template_part('template-parts/page-header'); ?>
-    <?php get_template_part('template-parts/breadcrumb'); ?>
-    <div class="main-container">
+    <main class="l-main">
+        <?php get_template_part('template-parts/page-header'); ?>
+        <?php get_template_part('template-parts/breadcrumb'); ?>
+        <div class="country-region-wrapper">
+            <section class="cr-section s-01">
+                <p class="text">
+                    留学も、年代によって様々な種類があります。これはサンプルのテキストです。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </section>
+            <!-- 高校生 -->
+            <article class="cr-article a-01">
+                <header class="cr-header">
+                    <h2 class="title">高校生の方へ</h2>
+                    <div class="image"
+                         style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/ca/header-bg-01.jpg')"></div>
+                </header>
 
-        <!-- 高校生 -->
-        <div class="age-forward">
-            留学も、年代によって様々な種類があります。これはサンプルのテキストです。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-        </div>
-    </div>
-    <div class="title-background">
-        <h1 class="age-title">
-            高校生の方へ
-        </h1>
-    </div>
-    <div class="main-container">
-        <div class="age-image">
+                <section class="cr-section s-01">
+                    <p class="text">
+                        高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。
+                    </p>
+                </section>
+            </article>
+            <!-- line -->
+            　
+            <div class="line-place">
+                <div class="age-option">
+                    <div class="age-line-button">
+                        <i class="fab fa-line"></i>
+                        <p class="open-win">LINEで相談する</p>
+                    </div>
+                </div>
+            </div>
+            <script src="<?php echo get_template_directory_uri(); ?>/assets/js/age.js"></script>
+            <!--
+                    <!-- 画像リンク -->
+            <div class="content-block">
+                <section class="image-link">
+                    <ul>
+                        <li class="pc-3c sp-2c i-03">
+                            <a href="<?php echo home_url('/purpose/short'); ?>">
+                                <div class="wrap">
+                                    <h1 class="title">短期留学</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/image-link-01.jpg')"></div>
+                            </a>
+                        </li>
+                        <li class="pc-3c sp-2c i-05">
+                            <a href="<?php echo home_url('/purpose/long'); ?>">
+                                <div class="wrap">
+                                    <h1 class="title">長期留学</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/image-link-02.jpg')"></div>
+                            </a>
+                        </li>
+                        <li class="pc-3c sp-2c i-03">
+                            <a href="<?php echo home_url('/purpose/working'); ?>">
+                                <div class="wrap">
+                                    <h1 class="holiday">ワーキングホリデー</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-holiday.jpg')"></div>
+                            </a>
+                        </li>
+                        <li class="pc-3c sp-2c i-05">
+                            <a href="<?php echo home_url('/purpose/language'); ?>">
+                                <div class="wrap">
+                                    <h1 class="title">語学留学</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/image-link-03.jpg')"></div>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+            <!-- 社会人 -->
+            <article class="cr-article a-02">
+                <header class="cr-header">
+                    <h2 class="title">社会人の方へ</h2>
+                    <div class="image"
+                         style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/ca/header-bg-02.jpg')"></div>
+                </header>
 
-        </div>
-        <p class="text">
-            高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。
-        </p>
-
-        <!-- line -->
-        <a href="" class="line">
-
-        </a>
-
-        <!-- 画像リンク -->
-        <section class="age-imagelink">
-            <ul>
-                <li class="pc-2c sp-2c i-02">
-                    <a href="<?php echo home_url('/about/new-style'); ?>">
-                        <div class="wrap">
-                            <h1 class="title">短期留学</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-02.jpg')"></div>
-                    </a>
-                </li>
-                <li class="pc-3c sp-2c i-03">
-                    <a href="<?php echo home_url('/about/new-style'); ?>">
-                        <div class="wrap">
-                            <h1 class="title">長期留学</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-03.jpg')"></div>
-                    </a>
-                </li>
-                <li class="pc-3c sp-2c i-04">
-                    <a href="<?php echo home_url('/about/new-style'); ?>">
-                        <div class="wrap">
-                            <h1 class="title">ワーキングホリデー</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-04.jpg')"></div>
-                    </a>
-                </li>
-                <li class="pc-3c sp-2c i-05">
-                    <a href="<?php echo home_url('/about/new-style'); ?>">
-                        <div class="wrap">
-                            <h1 class="title">語学留学</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-05.jpg')"></div>
-                    </a>
-                </li>
-            </ul>
-        </section>
-    </div>
-    <!-- 社会人 -->
-    <div class="title-background">
-        <h1 class="age-title">
-            社会人の方へ
-        </h1>
-    </div>
-    <div  class="main-container">
-        <div class="age-image">
-
-        </div>
-        <p class="text">
-            高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けのコンテンツですので、高校生向けのメッセージをご用意ください。
-        </p>
-
-        <!-- line -->
-        <a href="" class="line">
-
-        </a>
-
-        <!-- 画像リンク -->
-        <section class="age-imagelink">
-            <ul>
-                <li class="pc-2c sp-2c i-02">
-                    <a href="<?php echo home_url(''); ?>">
-                        <div class="wrap">
-                            <h1 class="title">短期留学</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-02.jpg')"></div>
-                    </a>
-                </li>
-                <li class="pc-3c sp-2c i-03">
-                    <a href="<?php echo home_url(''); ?>">
-                        <div class="wrap">
-                            <h1 class="title">長期留学</h1>
-                            <span class="text">テキストテキストテキストテキストテキスト</span>
-                        </div>
-                        <div class="bg-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/about/image-link-03.jpg')"></div>
-                    </a>
-                </li>
-            </ul>
-        </section>
-    </div>
-
-
-</main>
+                <section class="cr-section s-01">
+                    <p class="text">
+                        高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。高校生向けコンテンツですので、高校生向けのメッセージをご用意ください。
+                    </p>
+                </section>
+            </article>
+            <div class="content-block">
+                <section class="image-link-2">
+                    <ul>
+                        <li class="pc-3c sp-2c i-03">
+                            <a href="<?php echo home_url('/purpose/working'); ?>">
+                                <div class="wrap">
+                                    <h1 class="holiday">ワーキングホリデー</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/purpose/bg-holiday.jpg')"></div>
+                            </a>
+                        </li>
+                        <li class="pc-3c sp-2c i-05">
+                            <a href="<?php echo home_url('/purpose/language'); ?>">
+                                <div class="wrap">
+                                    <h1 class="title">語学留学</h1>
+                                </div>
+                                <div class="bg-image"
+                                     style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/country/usa/image-link-03.jpg')"></div>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+            <!-- line -->
+            <div class="line-place">
+                <div class="age-option">
+                    <div class="age-line-button">
+                        <i class="fab fa-line"></i>
+                        <p class="open-win">LINEで相談する</p>
+                    </div>
+                </div>
+            </div>
+            <script src="<?php echo get_template_directory_uri(); ?>/assets/js/age.js"></script>
+    </main>
 <?php get_footer(); ?>
-=======
-__( 'purpose', 'tcd-w' );
-get_header();
-?>
-<main class="l-main">
-    <?php get_template_part( 'template-parts/page-header' ); ?>
-    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
-
-    <div class="purpose-wrapper">
-        <p class="about">
-            準備中です。
-        </p>
-
-    </div>
-</main>
-<?php get_footer(); ?>
->>>>>>> cb29c71d82700aad607beef366e2cda56868d2be
